4bd98b6f9f9b27aa93c7c036a260b59eccc4ddba
Carsten Stann <carsten.stann@gmail.com>
2019-06-13 11:30:09 +0200

    update README

--- Panel_Regression.R
+++ Panel_Regression.R
@@ -1,27 +1,30 @@
 library(readxl)
 library(plm)
 library(lmtest)
+library(ggplot2)
+library(dplyr)
 
-state_names <- read_excel(path = "/Users/carsten 1/Desktop/A.F. International Economics and Area Studies/Workbook1.xlsx",
+# import data ---------------------------------------------------------------------------------------------------------------
+state_names <- read_excel(path = "/Users/Carsten/Documents/Freie Universität Berlin/WS 17_18/A.F. International Economics and Area Studies/Workbook1.xlsx",
                           sheet = "Medicare", col_names = FALSE, n_max =1, col_types = "text")
 
-medicare <- read_excel(path = "/Users/carsten 1/Desktop/A.F. International Economics and Area Studies/Workbook1.xlsx",
+medicare <- read_excel(path = "/Users/Carsten/Documents/Freie Universität Berlin/WS 17_18/A.F. International Economics and Area Studies/Workbook1.xlsx",
                        sheet = "Medicare", col_names = FALSE, col_types = "numeric", skip = 7)
 
-medicaid <- read_excel(path = "/Users/carsten 1/Desktop/A.F. International Economics and Area Studies/Workbook1.xlsx",
+medicaid <- read_excel(path = "/Users/Carsten/Documents/Freie Universität Berlin/WS 17_18/A.F. International Economics and Area Studies/Workbook1.xlsx",
                        sheet = "Medicaid", col_names = FALSE, skip = 7, col_types = "numeric")
 
-opioid_deaths <- read_excel(path ="/Users/carsten 1/Desktop/A.F. International Economics and Area Studies/PrescriptionOpioidDeaths.xlsx",
+opioid_deaths <- read_excel(path ="/Users/Carsten/Documents/Freie Universität Berlin/WS 17_18/A.F. International Economics and Area Studies/PrescriptionOpioidDeaths.xlsx",
                             sheet = "Sheet1", col_names = FALSE, col_types = "numeric", 
                             trim_ws = TRUE, skip = 7)
 
-gdp <- read_excel(path = "/Users/carsten 1/Desktop/A.F. International Economics and Area Studies/StateGDP.xls",
+gdp <- read_excel(path = "/Users/Carsten/Documents/Freie Universität Berlin/WS 17_18/A.F. International Economics and Area Studies/StateGDP.xls",
                   sheet = "Sheet1", col_names = FALSE, col_types = "numeric", skip = 7)
 
-rxrate <- read_excel(path = "/Users/carsten 1/Desktop/A.F. International Economics and Area Studies/CDCPrescribingRate.xlsx",
+rxrate <- read_excel(path = "/Users/Carsten/Documents/Freie Universität Berlin/WS 17_18/A.F. International Economics and Area Studies/CDCPrescribingRate.xlsx",
                      sheet = "Sheet2", col_names = FALSE, col_types = "numeric", skip = 1)
 
-elderly <- read_excel(path = "/Users/carsten 1/Desktop/A.F. International Economics and Area Studies/elderly.xls",
+elderly <- read_excel(path = "/Users/Carsten/Documents/Freie Universität Berlin/WS 17_18/A.F. International Economics and Area Studies/elderly.xls",
                       sheet = "Sheet2", col_names = FALSE, col_types = "numeric", skip = 7)
 
 medicare <- medicare[,-1]
@@ -33,6 +36,7 @@ elderly <- elderly[,-1]
 
 mm <- medicare + medicaid
 
+# form panel data -----------------------------------------------------------------------
 states_panel <- c()
 for (i in  1:44)
 {
@@ -105,7 +109,8 @@ pdata <- pdata.frame(x = panel, index = c("state", "year"))
 
 pool <- plm(log(deaths) ~ log(medicare_medicaid) + log(gdp)+ log (prescription_rate), data = pdata, 
             model = "pooling")
-##test for individual effects in the pooling model
+
+# test for individual effects in the pooling model
 plmtest(pool, "individual")
 
 
@@ -113,9 +118,11 @@ FEplm <- plm(log(deaths) ~ log(medicare_medicaid) + log(gdp) + log(prescription_
                  model = "within")
 REplm <- plm(log(deaths) ~ log(medicare) + log(gdp) + log(prescription_rate) + log(elderly), data = pdata, 
              model = "random")
+
 # compare FEplm and pool model for individual effects
 pFtest(FEplm,pool)
-## conduct hausmann test, reject null at 5 % level. Choose FE model for p less than .05
+
+# conduct hausmann test, reject null at 5 % level. Choose FE model for p less than .05
 hausman <- phtest(x = FEplm, x2 = REplm, model = c("within", "random"))
 hausman
 
@@ -127,26 +134,24 @@ FEplm2 <- plm(log(deaths) ~ log(medicare) + log(gdp), data = pdata,
 summary(FEplm)
 summary(FEplm2)
 
-##Test for Autocorrelation: Breusch-Godfrey Test
-## Null Hypothesis: No Serial Correlation. 
+# Test for Autocorrelation: Breusch-Godfrey Test
+# Null Hypothesis: No Serial Correlation. 
 pbgtest(FEplm)
 pbgtest(FEplm2)
 pbgtest(REplm)
+
 #Test for heteroskedasticity: heteterskedasticity present for p less than .05
 bptest(FEplm)
 bptest(FEplm2)
 bptest(REplm)
 
-##Because Serial Correlation and Heteroskedasticity present. Use Newey West HAC Errors
+# Because Serial Correlation and Heteroskedasticity present. Use Newey West HAC Errors
 coeftest(FEplm, vcov.=vcovNW)
 coeftest(FEplm2, vcov.=vcovNW)
 coeftest(REplm, vcov. =vcovNW)
 
-
-##Plots
-library(ggplot2)
-
-ggplot(pdata, mapping = aes(x = factor(state),y = medicare_medicaid/1000000, colour = year))+
+# Plots
+state_spending <- ggplot(pdata, mapping = aes(x = factor(state),y = medicare_medicaid/1000000, colour = year))+
   geom_point()+
   theme_minimal()+
   theme(plot.title = element_text(hjust = 0.5),
@@ -156,14 +161,15 @@ ggplot(pdata, mapping = aes(x = factor(state),y = medicare_medicaid/1000000, col
   labs(x = "State", y = "Spending (Millions of $)")+
   scale_y_continuous(labels=function(n){format(n, big.mark = ",", scientific = FALSE)})
   
-  
-  
+ggsave("StateSpending.png", 
+       plot = state_spending, 
+       device = "png", 
+       path = "/Users/Carsten/Documents/GitHub/Opiod_Crisis_Analysis/README_figs/",
+       width = 20, height = 10, units = "cm",
+       dpi = 320, limitsize = TRUE)
 
 
-  
-## Aggregate Data using dplyr
-
-library(dplyr)
+# Aggregate Data using dplyr
 a.deaths <- summarise(group_by(pdata, year), sum(deaths))
 colnames(a.deaths) <- c("year", "deaths")
 a.mm <- summarise(group_by(pdata, year), sum(medicare_medicaid))
@@ -174,7 +180,6 @@ a.rx <- summarise(group_by(pdata, year), sum(prescription_rate))
 colnames(a.rx) <- c("year", "rx")
 
 lm <- lm(log(a.deaths$deaths) ~ log(a.mm$mm) + log(a.gdp$gdp) + log(a.rx$rx))
-
 
 ggplot(a.deaths, mapping = aes(x = as.Date(year, format = "%Y"), y = deaths))+
   geom_line()
